89bdb817cf88878018cac254df9b5caac4c65ff2
Tak1za@Ubuntu <varungupta2015135@gmail.com>
2020-12-13 18:41:26 +0530

    Fixed hand dealing logic

--- main.go
+++ main.go
@@ -29,7 +29,6 @@ type Round struct {
 	cardsToBeDealt int
 	number         int
 	cards          map[string][]deck.Card
-	winner         Winner
 	dealt          map[string]deck.Card
 	handEstimate   map[string]int
 	trump          deck.Suit
@@ -62,24 +61,19 @@ func main() {
 		round.cardsToBeDealt = (52/numPlayers - i)
 		fmt.Println("Cards to be dealt: ", round.cardsToBeDealt)
 		round.dealCards(writer)
-		round.getHandEstimate(writer)
-		maxEstimatePlayer := "P0"
-		maxEstimate := round.handEstimate[maxEstimatePlayer]
-		for playerID, estimate := range round.handEstimate {
-			if estimate > maxEstimate {
-				maxEstimate = estimate
-				maxEstimatePlayer = playerID
-			}
-		}
+		maxEstimatePlayer := round.getHandEstimate(writer)
 		fmt.Printf("What do you want as the trump %s?: \n", maxEstimatePlayer)
 		for index, suit := range []string{"Spades", "Diamonds", "Clubs", "Hearts"} {
 			fmt.Printf("%s. %s\n", strconv.Itoa(index), suit)
 		}
 		fmt.Scanf("%d", &round.trump)
+		for round.trump >= 4 || round.trump < 0 {
+			fmt.Printf("Invalid trump. Please select again: ")
+			fmt.Scanf("%d", &round.trump)
+		}
+		fmt.Println("Trump for this round is: ", round.trump)
+
 		round.startRound()
-		order := []string{"P0", "P1", "P2", "P3", "P4"}
-		roundWinner := round.dealWinner(order)
-		fmt.Println("Deal Winner: ", roundWinner)
 	}
 }
 
@@ -111,15 +105,30 @@ func (r *Round) dealCards(w *csv.Writer) {
 	}
 }
 
-func (r *Round) getHandEstimate(w *csv.Writer) {
+func (r *Round) getHandEstimate(w *csv.Writer) string {
 	var handEstimate int
 	r.handEstimate = make(map[string]int, gs.Players)
 	scoringRow := (52 / gs.Players) + 1 - r.cardsToBeDealt
 	scoreData[scoringRow] = append(scoreData[scoringRow], fmt.Sprint(r.cardsToBeDealt))
+	maxEstimate := -1
+	maxEstimatePlayer := "P0"
+	handEstimateSum := 0
 	for i := 0; i < gs.Players; i++ {
 		playerID := "P" + strconv.Itoa(i)
 		fmt.Printf("How many hands can you make %s?: ", playerID)
 		fmt.Scanf("%d", &handEstimate)
+
+		for handEstimate > r.cardsToBeDealt || handEstimate < 0 {
+			fmt.Printf("Invalid estimate. Estimate again: ")
+			fmt.Scanf("%d", &handEstimate)
+		}
+		handEstimateSum += handEstimate
+
+		// find the player with maximum hands, will be trump setter
+		if handEstimate > maxEstimate {
+			maxEstimate = handEstimate
+			maxEstimatePlayer = playerID
+		}
 		r.handEstimate[playerID] = handEstimate
 		scoreData[scoringRow] = append(scoreData[scoringRow], fmt.Sprint(handEstimate))
 	}
@@ -129,31 +138,53 @@ func (r *Round) getHandEstimate(w *csv.Writer) {
 	if err != nil {
 		panic(err)
 	}
+
+	return maxEstimatePlayer
 }
 
 func (r *Round) startRound() {
 	fmt.Println("Round start...")
-	r.dealt = make(map[string]deck.Card)
-	for i := 0; i < gs.Players; i++ {
-		playerIndex := "P" + strconv.Itoa(i)
-		var cardPlayedNumber int
-		fmt.Println(playerIndex + "'s chance: ")
-		for j, card := range r.cards[playerIndex] {
-			fmt.Println(strconv.Itoa(j) + ". " + card.String())
+	for k := 0; k < r.cardsToBeDealt; k++ {
+		r.dealt = make(map[string]deck.Card, gs.Players)
+		for i := 0; i < gs.Players; i++ {
+			playerIndex := "P" + strconv.Itoa(i)
+			var cardPlayedNumber int
+			fmt.Println(playerIndex + "'s chance: ")
+			for j, card := range r.cards[playerIndex] {
+				fmt.Println(strconv.Itoa(j) + ". " + card.String())
+			}
+			fmt.Println("Choose one: ")
+			fmt.Scanf("%d", &cardPlayedNumber)
+			for cardPlayedNumber > r.cardsToBeDealt-k-1 || cardPlayedNumber < 0 {
+				fmt.Printf("Invalid card. Please select again: ")
+				fmt.Scanf("%d", &cardPlayedNumber)
+			}
+			cardPlayed := r.cards[playerIndex][cardPlayedNumber]
+			r.dealt[playerIndex] = cardPlayed
+			r.cards[playerIndex] = removeFromPlayerDeck(r.cards[playerIndex], cardPlayedNumber)
 		}
-		fmt.Println("Choose one: ")
-		fmt.Scanf("%d", &cardPlayedNumber)
-		cardPlayed := r.cards[playerIndex][cardPlayedNumber]
-		r.dealt[playerIndex] = cardPlayed
-	}
 
-	fmt.Println("Cards played this round: ")
-	for player, card := range r.dealt {
-		fmt.Printf("Player: %s, Card: %+v\n", player, card)
+		fmt.Println("Cards played this hand: ")
+		for player, card := range r.dealt {
+			fmt.Printf("Player: %s, Card: %+v\n", player, card)
+		}
+
+		order := []string{"P0", "P1", "P2", "P3", "P4"}
+		handWinner := r.handWinner(order)
+		fmt.Println("Hand Winner: ", handWinner)
+
 	}
 }
 
-func (r *Round) dealWinner(order []string) WinnerHand {
+// not making use of pointer to the playerDeck for sake of clean code
+func removeFromPlayerDeck(playerDeck []deck.Card, index int) []deck.Card {
+	copy(playerDeck[index:], playerDeck[index+1:])
+	playerDeck[len(playerDeck)-1] = deck.Card{}
+	playerDeck = playerDeck[:len(playerDeck)-1]
+	return playerDeck
+}
+
+func (r *Round) handWinner(order []string) WinnerHand {
 	winnerCard := r.dealt[order[0]]
 	winnerPlayer := order[0]
 	for i := 0; i < gs.Players-1; i++ {
